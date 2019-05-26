ec474c3afeba19dd257c9a2ed0ed547e03552ba7
iyatsouba <ivan@lanars.com>
2019-05-26 21:57:08 +0300

    Add helmet and rate limit to app

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,12 +1,14 @@
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe, UnprocessableEntityException } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
+import * as helmet from 'helmet';
+
+import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -26,6 +28,17 @@ async function bootstrap() {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  const rateLimit = require("express-rate-limit");
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  });
+
+  app.use(
+    limiter,
+    helmet());
+
   await app.listen(3000);
 }
 bootstrap();
